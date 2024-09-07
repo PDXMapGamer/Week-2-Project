1be7a85d489aa8f62a900367a1623b18bd3f761c
Thomas Harvey <thomasharvey1415@gmail.com>
2024-09-07 16:54:38 +0100

    Added indexing to the images, will be used for the foundation of future features.

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Bug 1: img.src and others were not getting values from element in the forEach lo
 Solution has been found open checking the name of the object properties and realising I was trying to call them by the wrong name.
 Bug 2: Multiple class names aren't being added. Solution was className = changes the class name instead of adding a new one so classList.add() fixes the problem.
 https://stackoverflow.com/questions/1988514/javascript-css-how-to-add-and-remove-multiple-css-classes-to-an-element.s
+https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //Test Comment
 let images = createImageArray();
-
+let imageIndex = 0;
+let globalIndex = 0;
 //STEP 1: Select DOM elements that will contain the images.
 const thumbnailContainer = document.getElementById("thumbnail-container");
 const mainImageContainer = document.getElementById("main-image-container");
@@ -14,6 +15,7 @@ function createThumbnails(thumbnailArray) {
     img.src = element.imageSrc; //assign the values
     img.alt = element.imageAlt;
     img.className = "thumbnail-image"; //give images class name
+    img.imageIndex = element.imageIndex = imageIndex; //sets both the value of img and element index to the current index, looks clunky but I am not sure how to make it prettier and it makes the code work
     //append image to the DOM
     thumbnailContainer.appendChild(img);
     //add event listener to display the clicked image in the main container.
@@ -21,16 +23,15 @@ function createThumbnails(thumbnailArray) {
       //set inner HTML to empty string.
       mainImageContainer.innerHTML = ""; //create img element
       const bigImg = document.createElement("img");
-      console.log(`Successful event, ${event}`);
       bigImg.src = element.imageSrc; //set the img values (src alt. width height are styled in css)
       bigImg.alt = element.imageAlt;
-
       bigImg.classList.add(element.imageOrientation); //give class
       bigImg.classList.add("big-image"); //using classList.add allows multiple classes to be added
-      //append to DOM
-      mainImageContainer.appendChild(bigImg);
+      globalIndex = element.imageIndex; //when img is cliked, it should set global index to the value of the current image's position in the array, eg image 3 is index 2.
+      mainImageContainer.appendChild(bigImg); //append to DOM
     });
-    // createLargeImage(element);
+    console.log(imageIndex);
+    imageIndex++; //at the end of the loop the image index increments, so it is equal to the position in the index.
   });
 }
 
@@ -75,5 +76,6 @@ function createSingleImage(src, alt, orientation) {
     imageSrc: src,
     imageAlt: alt,
     imageOrientation: orientation,
+    imageIndex: "",
   };
 }
